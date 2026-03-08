e8e98267a603cabad3a364957195b47905e19ffd
Keshama <keisyaspw@gmail.com>
2026-03-08 13:46:56 +0700

    menambahkan tampilan GUI kalkulator

--- TP_MODUL3_103022400060/Form1.cs
+++ TP_MODUL3_103022400060/Form1.cs
@@ -6,5 +6,40 @@ namespace TP_MODUL3_103022400060
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
